cd203ade12e2989de1f04dab0f8f933cb06c135b
Abdullah Tariq <78523853+Abdullahtariq11@users.noreply.github.com>
2026-06-03 23:02:41 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = 0; 
+        int sell = 1;
+        int maxProfit = 0;
+        while (sell < prices.length) {
+            if (prices[buy] < prices[sell]) {
+                int currentProfit = prices[sell] - prices[buy];
+                maxProfit = Math.max(maxProfit, currentProfit);
+            } else {
+                buy = sell;
+            }
+            sell++; 
+        }
+        return maxProfit;
+    }
+}
