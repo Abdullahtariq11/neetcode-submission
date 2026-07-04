2cc95f68e2873437e9debb853920f707d9fae76c
Abdullah Tariq <78523853+Abdullahtariq11@users.noreply.github.com>
2026-07-04 11:38:09 -0700

    Add: linked-list-cycle-detection - submission-6

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-6.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        HashMap<ListNode,Integer> hashSet=new HashMap<>();
+        while(slow!=null &&fast!=null && fast.next!=null){
+            fast=fast.next;
+            if(fast==slow){
+                return true;
+            }
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return false;
+        
+    }
+}
